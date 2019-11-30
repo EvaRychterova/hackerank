5954714f3836d55f127bb85284c8e5d69c0492ec
e.rychterova <e.rychterova@quadient.com>
2019-11-30 14:05:57 +0100

    first programming

--- src/easy/CompareTheTriplets/Solution.java
+++ src/easy/CompareTheTriplets/Solution.java
@@ -1,33 +1,41 @@
 package easy.CompareTheTriplets;
+
 import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.function.*;
-import java.util.regex.*;
-import java.util.stream.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 public class Solution {
 
+
     // Complete the compareTriplets function below.
     static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
-        int a1 = a.get(0);
-        int b1 = b.get(0);
-        int sumaAlice = 0;
-        if (a1>b1) {
-            sumaAlice = sumaAlice  + 1;
+
+        List<Integer> result = new ArrayList<>();
+        result.add(0, 0);
+        result.add(1, 0);
+
+        for (int i = 0; i < a.size(); i++) {
+            int pointsAlice = a.get(i);
+            int pointsBob = b.get(i);
+
+            if (pointsAlice != pointsBob) {
+                if (pointsAlice < pointsBob) {
+                    result.set(1, result.get(1) + 1);
+                } else {
+                    result.set(0, result.get(0) + 1);
+                }
+            }
         }
-
-
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH") + "/pokus.txt"));
 
         List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
                 .map(Integer::parseInt)
